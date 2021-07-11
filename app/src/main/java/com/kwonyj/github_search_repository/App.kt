8e574c99b06fe599b0d5f2b65847f6b97c2a4265
Yongjin <jrrnjs@gmail.com>
2021-07-11 14:42:51 +0900

    [feature] #2 Hilt // Hilt plugin 및 기본 초기화 코드 추가

---
+++ app/src/main/java/com/kwonyj/github_search_repository/App.kt
@@ -0,0 +1,7 @@
+package com.kwonyj.github_search_repository
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application()
--- app/src/main/java/com/kwonyj/github_search_repository/ui/MainActivity.kt
+++ app/src/main/java/com/kwonyj/github_search_repository/ui/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.kwonyj.github_search_repository
+package com.kwonyj.github_search_repository.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kwonyj.github_search_repository.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
