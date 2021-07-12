eee7bceffd26c9714713d99671b0ca9b1649a544
Yongjin <jrrnjs@gmail.com>
2021-07-13 00:05:55 +0900

    [featrue] #5 UI // EditText textChangedObservable 추가, text input 검색 이벤트 연결

--- app/src/main/java/com/kwonyj/github_search_repository/ui/MainActivity.kt
+++ app/src/main/java/com/kwonyj/github_search_repository/ui/MainActivity.kt
@@ -5,11 +5,19 @@ import androidx.activity.viewModels
 import com.kwonyj.github_search_repository.R
 import com.kwonyj.github_search_repository.databinding.ActivityMainBinding
 import com.kwonyj.github_search_repository.ext.initBinding
+import com.kwonyj.github_search_repository.util.textChangedObservable
 import dagger.hilt.android.AndroidEntryPoint
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import java.util.concurrent.TimeUnit
 
 @AndroidEntryPoint
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
 
+    private val compositeDisposable: CompositeDisposable by lazy {
+        CompositeDisposable()
+    }
     private val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,5 +31,20 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
         binding.rvRepositories.run {
             adapter = GithubRepoAdapter()
         }
+
+        binding.etSearch.textChangedObservable()
+            .debounce(500, TimeUnit.MILLISECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { keyword ->
+                (binding.rvRepositories.adapter as GithubRepoAdapter).setSearchKeyword(keyword)
+                viewModel.searchRepository(keyword, 1)
+            }.addTo(compositeDisposable)
+    }
+
+    override fun onDestroy() {
+        if (!compositeDisposable.isDisposed) {
+            compositeDisposable.dispose()
+        }
+        super.onDestroy()
     }
 }
